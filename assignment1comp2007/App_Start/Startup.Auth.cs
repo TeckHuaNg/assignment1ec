c93e09a9e1287f6d04f7e282560f8aa689260c7a
TeckHuaNg <200348264@student.georgianc.on.ca>
2018-03-28 16:42:59 -0400

    Added Facebook and Microsoft login

--- assignment1comp2007/App_Start/Startup.Auth.cs
+++ assignment1comp2007/App_Start/Startup.Auth.cs
@@ -46,14 +46,14 @@ namespace assignment1comp2007
             // This is similar to the RememberMe option when you log in.
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            //Uncomment the following lines to enable logging in with third party login providers
+            app.UseMicrosoftAccountAuthentication(
+                clientId: ConfigurationManager.AppSettings["MicrosoftClientId"],
+                clientSecret: ConfigurationManager.AppSettings["MicrosoftClientSecret"]);
 
-            app.UseTwitterAuthentication(
-               consumerKey: ConfigurationManager.AppSettings["TwitterConsumerKey"],
-               consumerSecret: ConfigurationManager.AppSettings["TwitterConsumerSecret"]);
+            //app.UseTwitterAuthentication(
+            //   consumerKey: ConfigurationManager.AppSettings["TwitterConsumerKey"],
+            //   consumerSecret: ConfigurationManager.AppSettings["TwitterConsumerSecret"]);
 
             //app.UseFacebookAuthentication(
             //   appId: "",
